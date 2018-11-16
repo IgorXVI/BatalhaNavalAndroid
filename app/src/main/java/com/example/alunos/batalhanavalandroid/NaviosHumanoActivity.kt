69be9a8dfb533111fe92a1ee272d59e6d744e892
Alunos <dkokoadkoasdkods>
2018-11-16 20:54:42 -0200

    posicionamento dos navios

--- app/src/main/java/com/example/alunos/batalhanavalandroid/NaviosHumanoActivity.kt
+++ app/src/main/java/com/example/alunos/batalhanavalandroid/NaviosHumanoActivity.kt
@@ -6,7 +6,7 @@ import android.view.View
 
 class NaviosHumanoActivity : AppCompatActivity() {
 
-
+    val g = Global.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,7 +14,12 @@ class NaviosHumanoActivity : AppCompatActivity() {
     }
 
     fun zerarPos(){
+        var navio: Navio
 
+        for(i in 2..4){
+            navio = g.humano.tabuleiro.getNavio(i)
+            navio.posInicial = navio.posFinal
+        }
     }
 
     fun posicionarNavio(view: View){
